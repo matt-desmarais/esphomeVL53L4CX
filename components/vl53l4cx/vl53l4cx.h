fe3ff63afea30003df0a47da2e9359bf8b9bc9c5
Matt <matt.desmarais78@gmail.com>
2024-10-11 02:40:19 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -1,23 +1,19 @@
 #pragma once
-#include "Wire.h"
-#include "esphome/components/i2c/i2c.h"
-#include "esphome/core/component.h"
+
 #include "esphome/components/sensor/sensor.h"
-#include "vl53l4cx_class.h"  // Include STM32Duino VL53L4CX library
+#include "esphome/components/i2c/i2c.h"
+#include "VL53L4CX.h"
 
 namespace esphome {
 namespace vl53l4cx {
 
-class VL53L4CXSensor : public PollingComponent, public sensor::Sensor, public i2c::I2CDevice {
+class VL53L4CXSensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
-  VL53L4CXSensor(uint32_t update_interval = 1000) : PollingComponent(update_interval) {}
-  
   void setup() override;
   void update() override;
 
  protected:
-  VL53L4CX sensor;  // Use the STM32Duino class
-  uint16_t get_distance();
+  VL53L4CX vl53l4cx_;
 };
 
 }  // namespace vl53l4cx
