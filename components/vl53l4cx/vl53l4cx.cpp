2f600cc2905ae4c7a85afa218faf29a44a338542
Matt <matt.desmarais78@gmail.com>
2024-10-10 23:22:15 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -5,7 +5,7 @@ namespace vl53l4cx {
 
 void VL53L4CXSensor::setup() {
   // Set I2C address and bus
-  this->set_i2c_address(0x29);
+// this->set_i2c_address(0x29);
 //  this->set_i2c_bus(i2c_arduinoi2cbus_id);  // Replace with the actual bus ID if needed
 
   // Sensor initialization logic
