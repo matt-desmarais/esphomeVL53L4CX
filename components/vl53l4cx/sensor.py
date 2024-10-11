1419c828d0ea2ec37f95a4af9e00ea07b5158eba
Matt <matt.desmarais78@gmail.com>
2024-10-11 04:08:16 -0400

    Update sensor.py

--- components/vl53l4cx/sensor.py
+++ components/vl53l4cx/sensor.py
@@ -16,4 +16,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 #    await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
-    await i2c.register_i2c_device(var, config)
+#    await i2c.register_i2c_device(var, config)
