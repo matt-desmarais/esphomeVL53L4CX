8d46dc409d136dcbca54a079ce9ed45ad77af8c0
Matt <matt.desmarais78@gmail.com>
2024-10-11 01:06:53 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -1,34 +1,36 @@
 #include "vl53l4cx.h"
-#include "esphome/core/log.h"
-#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace vl53l4cx {
 
 void VL53L4CXSensor::setup() {
-  // Sensor initialization logic
-  ESP_LOGI("VL53L4CX", "Initializing VL53L4CX sensor...");
+  Wire.begin();
+  delay(500);  // Add a small delay after I2C initialization
 
-  // Initialize the sensor using I2C
-  if (this->sensor.VL53L4CX_DataInit() != 0) {
+  Wire.beginTransmission(0x29);  // Assuming sensor address is 0x29
+  uint8_t error = Wire.endTransmission();
+  
+  if (error != 0) {
+    ESP_LOGE("VL53L4CX", "I2C communication failed! Error code: %d", error);
+    return;
+  }
+
+  if (sensor.VL53L4CX_DataInit() != 0) {
     ESP_LOGE("VL53L4CX", "Sensor initialization failed!");
     return;
   }
 
-  // Set tuning parameter
-  if (this->sensor.VL53L4CX_SetTuningParameter(1, 50) != 0) {
+  if (sensor.VL53L4CX_SetTuningParameter(1, 50) != 0) {
     ESP_LOGE("VL53L4CX", "Failed to set tuning parameter.");
     return;
   }
 
-  // Set distance mode to long range
-  if (this->sensor.VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG) != 0) {
+  if (sensor.VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG) != 0) {
     ESP_LOGE("VL53L4CX", "Failed to set distance mode.");
     return;
   }
 
-  // Start the sensor measurement
-  if (this->sensor.VL53L4CX_StartMeasurement() != 0) {
+  if (sensor.VL53L4CX_StartMeasurement() != 0) {
     ESP_LOGE("VL53L4CX", "Failed to start measurement.");
     return;
   }
@@ -37,41 +39,50 @@ void VL53L4CXSensor::setup() {
 }
 
 void VL53L4CXSensor::update() {
-  // Read the distance value
   uint16_t distance = get_distance();
 
-  // Publish the distance if valid, otherwise publish NAN
   if (distance > 0) {
-    this->publish_state(distance);
+    publish_state(distance);  // Publish the distance in mm
   } else {
     ESP_LOGE("VL53L4CX", "Invalid measurement, publishing NAN");
-    this->publish_state(NAN);
+    publish_state(NAN);  // Publish NAN if measurement is invalid
   }
 }
 
 uint16_t VL53L4CXSensor::get_distance() {
   VL53L4CX_MultiRangingData_t ranging_data;
+  sensor.VL53L4CX_GetMultiRangingData(&ranging_data);
 
-  // Use the sensor's API to get ranging data
-  if (this->sensor.VL53L4CX_GetMultiRangingData(&ranging_data) == 0) {
-    if (ranging_data.NumberOfObjectsFound > 0) {
-      uint16_t min_distance = ranging_data.RangeData[0].RangeMilliMeter;
+  if (ranging_data.NumberOfObjectsFound > 0) {
+    // Initialize the minimum distance to the first object's distance
+    uint16_t min_distance = ranging_data.RangeData[0].RangeMilliMeter;
 
-      // Find the minimum distance if multiple objects are found
-      for (int i = 1; i < ranging_data.NumberOfObjectsFound; i++) {
-        uint16_t current_distance = ranging_data.RangeData[i].RangeMilliMeter;
-        if (current_distance < min_distance) {
-          min_distance = current_distance;
-        }
+    // Loop through all detected objects and find the minimum distance
+    for (int i = 1; i < ranging_data.NumberOfObjectsFound; i++) {
+      uint16_t current_distance = ranging_data.RangeData[i].RangeMilliMeter;
+      if (current_distance < min_distance) {
+        min_distance = current_distance;  // Update the minimum distance
       }
-
-      return min_distance;
     }
-  }
 
-  // Return 0 if no objects were found
-  return 0;
+    return min_distance;  // Return the shortest distance
+  } else {
+    return 0;  // Return 0 if no object is found
+  }
 }
 
+
+/*
+uint16_t VL53L4CXSensor::get_distance() {
+  VL53L4CX_MultiRangingData_t ranging_data;
+  sensor.VL53L4CX_GetMultiRangingData(&ranging_data);
+
+  if (ranging_data.NumberOfObjectsFound > 0) {
+    return ranging_data.RangeData[0].RangeMilliMeter;  // Return the first object's distance
+  } else {
+    return 0;  // Return 0 if no object is found
+  }
+}
+*/
 }  // namespace vl53l4cx
 }  // namespace esphome
