dc745532ed728278e081b151f49ddcf0fa29e02d
Matt <matt.desmarais78@gmail.com>
2024-10-10 22:28:20 -0400

    Update __init__.py

--- components/vl53l4cx/__init__.py
+++ components/vl53l4cx/__init__.py
@@ -6,9 +6,11 @@ from esphome.const import UNIT_MILLIMETER, ICON_RULER, CONF_ID
 vl53l4cx_ns = cg.esphome_ns.namespace('vl53l4cx')
 VL53L4CXSensor = vl53l4cx_ns.class_('VL53L4CXSensor', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_MILLIMETER, ICON_RULER, 0).extend({
-    cv.generate_id(): cv.declare_id(VL53L4CXSensor),
-}).extend(cv.polling_component_schema('1s'))
+CONFIG_SCHEMA = sensor.sensor_schema().extend({
+    cv.Optional(CONF_UNIT_OF_MEASUREMENT, default=UNIT_MILLIMETER): cv.string,
+    cv.Optional(CONF_ICON, default=ICON_RULER): cv.icon,
+    cv.Optional(CONF_ACCURACY_DECIMALS, default=0): cv.int_,
+}).extend(cv.polling_component_schema('60s')).extend(i2c.I2C_DEVICE_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
