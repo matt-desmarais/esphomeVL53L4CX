dee427736627b097612148393cb1f1a7305b2a83
Matt <matt.desmarais78@gmail.com>
2024-10-11 00:37:33 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -1,27 +1,34 @@
 #include "vl53l4cx.h"
+#include "esphome/core/log.h"
+#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace vl53l4cx {
 
 void VL53L4CXSensor::setup() {
-
   // Sensor initialization logic
-  if (sensor.VL53L4CX_DataInit() != 0) {
+  ESP_LOGI("VL53L4CX", "Initializing VL53L4CX sensor...");
+
+  // Initialize the sensor using I2C
+  if (this->VL53L4CX_DataInit() != 0) {
     ESP_LOGE("VL53L4CX", "Sensor initialization failed!");
     return;
   }
 
-  if (sensor.VL53L4CX_SetTuningParameter(1, 50) != 0) {
+  // Set tuning parameter
+  if (this->VL53L4CX_SetTuningParameter(1, 50) != 0) {
     ESP_LOGE("VL53L4CX", "Failed to set tuning parameter.");
     return;
   }
 
-  if (sensor.VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG) != 0) {
+  // Set distance mode to long range
+  if (this->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG) != 0) {
     ESP_LOGE("VL53L4CX", "Failed to set distance mode.");
     return;
   }
 
-  if (sensor.VL53L4CX_StartMeasurement() != 0) {
+  // Start the sensor measurement
+  if (this->VL53L4CX_StartMeasurement() != 0) {
     ESP_LOGE("VL53L4CX", "Failed to start measurement.");
     return;
   }
@@ -30,34 +37,40 @@ void VL53L4CXSensor::setup() {
 }
 
 void VL53L4CXSensor::update() {
+  // Read the distance value
   uint16_t distance = get_distance();
 
+  // Publish the distance if valid, otherwise publish NAN
   if (distance > 0) {
-    publish_state(distance);
+    this->publish_state(distance);
   } else {
     ESP_LOGE("VL53L4CX", "Invalid measurement, publishing NAN");
-    publish_state(NAN);
+    this->publish_state(NAN);
   }
 }
 
 uint16_t VL53L4CXSensor::get_distance() {
   VL53L4CX_MultiRangingData_t ranging_data;
-  sensor.VL53L4CX_GetMultiRangingData(&ranging_data);
 
-  if (ranging_data.NumberOfObjectsFound > 0) {
-    uint16_t min_distance = ranging_data.RangeData[0].RangeMilliMeter;
+  // Use the sensor's API to get ranging data
+  if (this->VL53L4CX_GetMultiRangingData(&ranging_data) == 0) {
+    if (ranging_data.NumberOfObjectsFound > 0) {
+      uint16_t min_distance = ranging_data.RangeData[0].RangeMilliMeter;
 
-    for (int i = 1; i < ranging_data.NumberOfObjectsFound; i++) {
-      uint16_t current_distance = ranging_data.RangeData[i].RangeMilliMeter;
-      if (current_distance < min_distance) {
-        min_distance = current_distance;
+      // Find the minimum distance if multiple objects are found
+      for (int i = 1; i < ranging_data.NumberOfObjectsFound; i++) {
+        uint16_t current_distance = ranging_data.RangeData[i].RangeMilliMeter;
+        if (current_distance < min_distance) {
+          min_distance = current_distance;
+        }
       }
-    }
 
-    return min_distance;
-  } else {
-    return 0;
+      return min_distance;
+    }
   }
+
+  // Return 0 if no objects were found
+  return 0;
 }
 
 }  // namespace vl53l4cx
