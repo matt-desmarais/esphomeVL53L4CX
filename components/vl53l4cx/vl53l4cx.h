b0fc1fed8b3951977c1e3f631eb33462a41d7737
Matt <matt.desmarais78@gmail.com>
2024-10-12 21:05:01 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -18,7 +18,7 @@ class VL53L4CXSensor : public sensor::Sensor, public PollingComponent, public i2
 
   //void set_update_interval(uint32_t interval) { this->update_interval_ = interval; }
   // Add constructor for setting update interval
-  void set_update_interval(uint32_t interval) { this->set_interval(interval); }
+  //void set_update_interval(uint32_t interval) { this->set_interval(interval); }
 
 
  protected:
