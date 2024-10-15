96ec5ce63b265181ffb8bc977cb72fd615275b18
Matt <matt.desmarais78@gmail.com>
2024-10-15 16:46:48 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -8,7 +8,8 @@ namespace vl53l4cx {
 static const char *TAG = "vl53l4cx.sensor";
 
 // Constructor
-VL53L4CXSensor::VL53L4CXSensor(uint32_t update_interval) : PollingComponent(update_interval), sensor_vl53l4cx_(&Wire, A1) {}
+VL53L4CXSensor::VL53L4CXSensor(uint32_t update_interval) : PollingComponent(update_interval), sensor_vl53l4cx_(nullptr) {}
+//VL53L4CXSensor::VL53L4CXSensor(uint32_t update_interval) : PollingComponent(update_interval), sensor_vl53l4cx_(&Wire, A1) {}
 // Default constructor
 VL53L4CXSensor::VL53L4CXSensor() : PollingComponent(15000), sensor_vl53l4cx_(&Wire, A1) {}  // Assuming a default update interval of 15 seconds
 
