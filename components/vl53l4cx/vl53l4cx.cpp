9bb45e7eb7f70b6dfa7ca1d46379a82a9131a073
Matt <matt.desmarais78@gmail.com>
2024-10-15 13:59:51 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -59,7 +59,7 @@ uint16_t VL53L4CXSensor::get_distance() {
     return 0;
   }
 }
-/8 In vl53l4cx.cpp
+/* In vl53l4cx.cpp
 void VL53L4CXSensor::set_i2c_bus(i2c::I2CBus *bus) {
   // Do nothing (I2C is already handled by the i2c_device_schema)
 }
