fbc3bdbe7fb311ce0d2f62d265699e4d94fb2f86
Matt <matt.desmarais78@gmail.com>
2024-10-15 16:42:42 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -19,7 +19,7 @@ void VL53L4CXSensor::setup() {
 
   // Initialize I2C
   Wire.begin();
-
+  this->sensor_vl53l4cx_ = new VL53L4CX(&Wire, this->address_);  // Changed to allocate on the heap
   // Initialize the sensor.
   this->sensor_vl53l4cx_.begin();
   this->sensor_vl53l4cx_.VL53L4CX_Off();
@@ -37,17 +37,22 @@ void VL53L4CXSensor::setup() {
 
 // Update function (reads the distance and publishes it)
 void VL53L4CXSensor::update() {
+  int status = this->sensor_vl53l4cx_->VL53L4CX_GetMeasurementDataReady(&NewDataReady);
   VL53L4CX_MultiRangingData_t ranging_data;
   uint8_t new_data_ready = 0;
+  this->sensor_vl53l4cx_ = new VL53L4CX(&Wire, this->address_);  // Changed to allocate on the heap
 
   // Check if new data is ready
   if (sensor_vl53l4cx_.VL53L4CX_GetMeasurementDataReady(&new_data_ready) != 0 || !new_data_ready) {
     ESP_LOGE(TAG, "No new data from VL53L4CX sensor.");
     return;
   }
+  if (status == 0 && new_data_ready != 0) {
+    VL53L4CX_MultiRangingData_t MultiRangingData;
+    this->sensor_vl53l4cx_->VL53L4CX_GetMultiRangingData(&MultiRangingData);  // Access pointer
 
   // Read the distance data
-  if (sensor_vl53l4cx_.VL53L4CX_GetMultiRangingData(&ranging_data) == 0 && ranging_data.NumberOfObjectsFound > 0) {
+//  if (this->sensor_vl53l4cx_->VL53L4CX_GetMultiRangingData(&ranging_data) == 0 && ranging_data.NumberOfObjectsFound > 0) {
     uint16_t distance = ranging_data.RangeData[0].RangeMilliMeter;
     ESP_LOGD(TAG, "Distance: %d mm", distance);
     this->publish_state(static_cast<float>(distance));
