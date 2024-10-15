cbb2e35fe71bdf8c08eaede2b86d6d7ea31c9a81
Matt <matt.desmarais78@gmail.com>
2024-10-15 14:26:40 -0400

    Update sensor.py

--- components/vl53l4cx/sensor.py
+++ components/vl53l4cx/sensor.py
@@ -7,14 +7,21 @@ from esphome.const import CONF_ID, UNIT_MILLIMETER, ICON_RULER, DEVICE_CLASS_DIS
 vl53l4cx_ns = cg.esphome_ns.namespace('vl53l4cx')
 VL53L4CXSensor = vl53l4cx_ns.class_('VL53L4CXSensor', sensor.Sensor, cg.PollingComponent, i2c.I2CDevice)
 
-# Configuration schema for the VL53L4CX sensor
-CONFIG_SCHEMA = cv.Schema({
+# Define the CONFIG_SCHEMA
+CONFIG_SCHEMA = sensor.sensor_schema(
+    VL53L4CXSensor,
+    unit_of_measurement=UNIT_MILLIMETER,
+    accuracy_decimals=1,
+    icon=ICON_RULER,
+    device_class=DEVICE_CLASS_DISTANCE,
+).extend({
     cv.GenerateID(): cv.declare_id(VL53L4CXSensor),
-}).extend(sensor.SENSOR_SCHEMA).extend(i2c.i2c_device_schema(0x29))  # VL53L4CX I2C address is usually 0x29
+    cv.Optional('address', default=0x29): cv.i2c_address,  # Default I2C address for VL53L4CX
+}).extend(i2c.i2c_device_schema(0x29))
 
-# Code generation function for the custom sensor
+# Code generation function for VL53L4CX sensor
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    # Register the component only once
     await sensor.register_sensor(var, config)
     await i2c.register_i2c_device(var, config)
