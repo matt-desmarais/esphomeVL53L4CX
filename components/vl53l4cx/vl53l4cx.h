a1619bb8f86563f6a47ecfcafb2384ff49afe37a
Matt <matt.desmarais78@gmail.com>
2024-10-15 14:58:42 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -4,6 +4,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
+#include "vl53l4cx_class.h"
 
 namespace esphome {
 namespace vl53l4cx {
