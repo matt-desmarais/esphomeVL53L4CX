a0a0b54b1320392f7703bde9894f9a8b0225420d
Matt <matt.desmarais78@gmail.com>
2024-10-15 15:42:32 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -4,7 +4,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
-#include "vl53l4cx_class.h"
+#include "vl53l4cx_class.h"  // Include the class from the VL53L4CX library
 
 namespace esphome {
 namespace vl53l4cx {
@@ -12,15 +12,12 @@ namespace vl53l4cx {
 class VL53L4CXSensor : public PollingComponent, public sensor::Sensor, public i2c::I2CDevice {
  public:
   VL53L4CXSensor(uint32_t update_interval);
-  VL53L4CXSensor();
   void setup() override;   // Setup function for initialization
   void update() override;  // Update function for reading sensor data
 
  protected:
-  // Function to read distance from VL53L4CX sensor
   float get_distance();
-  //VL53L4CX vl53l4cx_; 
-  VL53L4CX vl53l4cx_;
+  VL53L4CX sensor_vl53l4cx_;
 };
 
 }  // namespace vl53l4cx
