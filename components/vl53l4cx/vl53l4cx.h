ffbddc3f00c4698ef2a111bbcce7c1d2bf3e1bf4
Matt <matt.desmarais78@gmail.com>
2024-10-15 16:38:46 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -11,7 +11,9 @@ namespace vl53l4cx {
 
 class VL53L4CXSensor : public PollingComponent, public sensor::Sensor, public i2c::I2CDevice {
  public:
-  VL53L4CXSensor();
+  VL53L4CXSensor() : sensor_vl53l4cx_(nullptr) {}  // Changed to pointer and initialized to nullptr
+  ~VL53L4CXSensor() { delete this->sensor_vl53l4cx_; }  // Added destructor to free memory
+  //VL53L4CXSensor();
   VL53L4CXSensor(uint32_t update_interval);
   void setup() override;   // Setup function for initialization
   void update() override;  // Update function for reading sensor data
