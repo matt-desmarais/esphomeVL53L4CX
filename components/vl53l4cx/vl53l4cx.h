f185d703cd1b41b4c0b49f8c91c58ec699497cb5
Matt <matt.desmarais78@gmail.com>
2024-10-15 14:36:22 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -11,7 +11,7 @@ namespace vl53l4cx {
 class VL53L4CXSensor : public PollingComponent, public sensor::Sensor, public i2c::I2CDevice {
  public:
   VL53L4CXSensor(uint32_t update_interval);
-
+  VL53L4CXSensor();
   void setup() override;   // Setup function for initialization
   void update() override;  // Update function for reading sensor data
 
