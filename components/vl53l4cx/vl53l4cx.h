2733d9e723033b9d0956907e75ef541cbae71b1d
Matt <matt.desmarais78@gmail.com>
2024-10-15 22:02:36 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -21,8 +21,16 @@ class VL53L4CXSensor : public PollingComponent, public sensor::Sensor {
   // This will be called to set up the hardware (sensor)
   float get_setup_priority() const override;
 
+  // Function to set the I2C bus
+  void set_i2c_bus(I2CComponent *i2c_bus) { this->i2c_bus_ = i2c_bus; }
+
+  // Function to set the I2C address
+  void set_i2c_address(uint8_t address) { this->i2c_address_ = address; }
+
  private:
   static VL53L4CX *sensor_instance;  // Declare the static sensor instance
+  I2CComponent *i2c_bus_;  // Store the I2C bus object
+  uint8_t i2c_address_;    // Store the I2C address
 };
 
 }  // namespace vl53l4cx
