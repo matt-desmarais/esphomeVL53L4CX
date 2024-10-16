78b40ed36d34704ccf39942baaedebd8c3dc438b
Matt <matt.desmarais78@gmail.com>
2024-10-15 22:04:21 -0400

    Update vl53l4cx.h

--- components/vl53l4cx/vl53l4cx.h
+++ components/vl53l4cx/vl53l4cx.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"  // Include the ESPHome I2C component
 #include "vl53l4cx_class.h"  // Include the VL53L4CX sensor class
 
 namespace esphome {
@@ -22,14 +23,14 @@ class VL53L4CXSensor : public PollingComponent, public sensor::Sensor {
   float get_setup_priority() const override;
 
   // Function to set the I2C bus
-  void set_i2c_bus(I2CComponent *i2c_bus) { this->i2c_bus_ = i2c_bus; }
+  void set_i2c_bus(i2c::I2CBus *i2c_bus) { this->i2c_bus_ = i2c_bus; }
 
   // Function to set the I2C address
   void set_i2c_address(uint8_t address) { this->i2c_address_ = address; }
 
  private:
   static VL53L4CX *sensor_instance;  // Declare the static sensor instance
-  I2CComponent *i2c_bus_;  // Store the I2C bus object
+  i2c::I2CBus *i2c_bus_;  // Store the I2C bus object
   uint8_t i2c_address_;    // Store the I2C address
 };
 
