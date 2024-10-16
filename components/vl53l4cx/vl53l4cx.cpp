4eddb0e3de17455b85cf87ac7cdadc90b5b1c4de
Matt <matt.desmarais78@gmail.com>
2024-10-15 22:48:22 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -84,3 +84,5 @@ float VL53L4CXSensor::get_setup_priority() const {
 
 }  // namespace vl53l4cx
 }  // namespace esphome
+// Ensure the component is registered in ESPHome
+esphome::Application::register_component(new esphome::vl53l4cx::VL53L4CXSensor());
