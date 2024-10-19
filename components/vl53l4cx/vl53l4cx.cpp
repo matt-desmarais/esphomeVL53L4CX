6534a1113f0cf45558ad3121df062cc8ecd1b391
Matt <matt.desmarais78@gmail.com>
2024-10-18 22:54:10 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -44,7 +44,7 @@ void VL53L4CXSensor::setup() {
   sensor_instance->VL53L4CX_StartMeasurement();
 
   //sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(200000);  // Set timing budget to 100ms
-  //sensor_instance->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_MEDIUM);  // Switch to long-range mode
+  sensor_instance->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG);  // Switch to long-range mode
 
   ESP_LOGI(TAG, "VL53L4CX setup complete.");
 }
