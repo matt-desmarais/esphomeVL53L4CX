8050b4bd6640b63c2222b453426a4fe3dd4cc1b4
Matt <matt.desmarais78@gmail.com>
2025-04-24 13:20:40 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -42,8 +42,11 @@ void VL53L4CXSensor::setup() {
 
   // Start continuous measurements
   sensor_instance->VL53L4CX_StartMeasurement();
+  sensor_instance->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG);
+  sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(80000);  // Better than 250ms but still reliable
+  sensor_instance->VL53L4CX_SetInterMeasurementPeriodMilliSeconds(100);     // Must be > timing budget/1000
 
-  sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(250000);  // Set timing budget to 100ms
+  //sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(250000);  // Set timing budget to 100ms
   // Set the timing budget (in microseconds)
   //sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(50000);  // 50ms timing budget
   //sensor_instance->VL53L4CX_SetInterMeasurementPeriodMilliSeconds(55);  // Set to 55ms
@@ -55,7 +58,7 @@ void VL53L4CXSensor::setup() {
     ESP_LOGI(TAG, "VL53L4CX sensor cross-talk compensation set to %.2f kcps.", xtalk_compensation);
   }
 
-  sensor_instance->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG);  // Switch to long-range mode
+  //sensor_instance->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG);  // Switch to long-range mode
   
   ESP_LOGI(TAG, "VL53L4CX setup complete.");
 }
