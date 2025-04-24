ef104507453a4e9410f6cf1654ece785d616eac1
Matt <matt.desmarais78@gmail.com>
2025-04-24 13:27:55 -0400

    Update vl53l4cx.cpp

--- components/vl53l4cx/vl53l4cx.cpp
+++ components/vl53l4cx/vl53l4cx.cpp
@@ -45,7 +45,7 @@ void VL53L4CXSensor::setup() {
   sensor_instance->VL53L4CX_SetDistanceMode(VL53L4CX_DISTANCEMODE_LONG);
   sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(80000);  // Better than 250ms but still reliable
   //sensor_instance->VL53L4CX_set_inter_measurement_period_ms(100);     // Must be > timing budget/1000
-  sensor_instance->SetInterMeasurementPeriodMs(100);
+  sensor_instance->VL53L4CX_SetInterMeasurementPeriodMs(100);
   //sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(250000);  // Set timing budget to 100ms
   // Set the timing budget (in microseconds)
   //sensor_instance->VL53L4CX_SetMeasurementTimingBudgetMicroSeconds(50000);  // 50ms timing budget
